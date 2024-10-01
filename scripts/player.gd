27cd976d96816e864f10b446db74bc9bf3ab6b1b
martinbriebac <105032631+martinbriebac@users.noreply.github.com>
2024-10-01 18:54:37 -0300

    created project in godot, added a main scene with a tilemap and a player scene with an animated sprite 2D, a collision shape 2D and a camera 2D attached to it. also created a sprite for the player and a tileset for the terrain

---
+++ scripts/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * SPEED
+	move_and_slide()
